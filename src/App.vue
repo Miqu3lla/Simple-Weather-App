d39b56a1dceb3a8640beaa5dab2aa418889f131b
mikeru <javierjaypee8@gmail.com>
2025-11-07 20:32:58 +0800

    add geocode api

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,34 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 import { getWeatherData } from '@/services/weatherAPI';
+import Errors from '@/views/errors.vue';
 
-const city = ref('Manila');
+const city = ref('');
 const weather = ref(null);
 const temp = ref(null);
 const loading = ref(false);
 const error = ref(null);
 
+
+
 onMounted(async () => {
   loading.value = true;
   try {
     weather.value = await getWeatherData(city.value);
-    temp.value = weather.value.main;
+    temp.value = weather.value.current.temp;
+    const celsius = temp.value - 273.15;
+    temp.value = celsius.toFixed(2);
   } catch (err) {
     error.value = err;
   } finally {
     loading.value = false;
   }
   })
-
 </script>
 
 <template>
-    <div v-if="loading" class = "flex items-center justify-center min-h-screen">
-        <h1 class = "Text-lg">loading...</h1>
+    <Errors></Errors>
+    <div>
+       
     </div>
-    <div v-if="error" class = "flex items-center justify-center">
-        <div class="Text-lg ">Error: {{ error.message }}</div>
-    </div>
-    <h1>{{ city }}</h1>
-    <h1>{{ temp }}</h1>
 </template>
--- src/services/weatherAPI.js
+++ src/services/weatherAPI.js
@@ -1,14 +1,16 @@
 const key = import.meta.env.VITE_WEATHER_KEY;
-
+const lat = '14.59948914';
+const lon = '120.9782618'
 export async function getWeatherData(city) {
     try {
 
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`);
+        const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${key}`);
         if (!response.ok) {
             throw new Error('Failed to fetch weather data');
         }
         const data = await response.json();
-        console.log(data)
+        console.log(data);
+        
         return data;
     }
     catch (error) {
