bebe7e726c4d0e93fd3d5f7a94e66f264b826582
mikeru <javierjaypee8@gmail.com>
2025-11-08 14:35:33 +0800

    add display error message and card style design

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { ref } from 'vue';
 import { getWeatherData } from '@/services/weatherAPI';
-import Errors from '@/views/errors.vue';
+import { Icon } from '@iconify/vue'
 
-const city = ref('');
-const weather = ref(null);
+const city = ref('Manila');
+const data = ref(null);
 const temp = ref(null);
 const loading = ref(false);
 const error = ref(null);
+const cityName = ref('');
+const weather = ref(null);
+
+
 async function fetchWeather() {
   const q = city.value.trim();
   if (!q) return;
   loading.value = true;
   try {
-    weather.value = await getWeatherData(city.value);
-    temp.value = weather.value.current.temp;
+    data.value = await getWeatherData(city.value);
+    cityName.value = city.value.trim();
+    temp.value = data.value.current.temp;
     temp.value = (temp.value - 273.15).toFixed(2);
+    weather.value = data.value.current.weather[0].main;
+
   } catch (err) {
     error.value = err;
   } finally {
@@ -27,14 +34,32 @@ async function fetchWeather() {
 </script>
 
 <template>
-    <Errors></Errors>
-    <div>
-      <form @submit.prevent="fetchWeather">
-
-        <input type="text" v-model="city" placeholder="Enter city name" class = "border"/>
-        <button type="submit" class="ml-2 p-1 bg-blue-500 text-white rounded">Get Weather</button>
-      </form>
+  <div class="text-center my-20 ">
+    <h1 class="text-3xl font-semibold mb-4">Weather App</h1>
+    <h1 class="text-gray-500"> Your daily weather companion</h1>
+  </div>
+  <div class="border-gray shadow-md rounded-xl p-6 w-50 mx-auto h-500 sm:w-75 md:w-150 lg:w-250">
+    <form @submit.prevent="fetchWeather" class="flex items-center justify-center mt-5">
+      <input type="text" v-model="city" placeholder="Search location" class="border-gray shadow-md rounded-lg w-50  h-10 md:w-100 lg:w-150" />
+      <button type="submit" class="ml-2 p-1 bg-black text-white rounded-full h-10 w-10">
+        <Icon icon="solar:magnifer-linear" class="text-white w-8 h-4" />
+      </button>
+    </form>
+<h1 v-if ="error" class = "text-red-400 ml-1 sm: ml-15"> Error:{{ error.message }}</h1>
+    <div class="text-center mt-15">
+      <h1 class="text-xl font-semibold">{{ cityName }}</h1>
+      <!-- icon + temp side-by-side -->
+      <div class="mt-2 flex items-center justify-center gap-2">
+        <Icon v-if="temp >= 30" icon="solar:sun-line-duotone" class="w-8 h-8" />
+        <Icon v-else-if="temp >= 20 && temp < 30" icon="solar:cloud-sun-line-duotone" class="w-8 h-8"/>
+        <Icon v-else-if="temp >= 10 && temp < 20" icon="solar:cloud-line-duotone" class="w-8 h-8" />
+        <Icon v-else-if="temp >= 10 && temp <= 15" icon="solar:cloud-rain-broken" class="w-8 h-8" />
+        <Icon v-else icon="solar:snowflake-line-duotone" class="w-8 h-8" />
+        <span class="text-xl font-semibold">{{ temp }} °C</span>
+      </div>
+      <div class="mt-2 flex items-center justify-center gap-2">
+        <h1 class="text-md font-semi">{{ weather }}</h1>
+      </div>
     </div>
-    <div>
-    </div>
+  </div>
 </template>
--- src/services/weatherAPI.js
+++ src/services/weatherAPI.js
@@ -1,7 +1,8 @@
 const key = import.meta.env.VITE_WEATHER_KEY;
 
 export async function getWeatherData(city) {
-
+    try {
+        if (!key) throw new Error('API key is missing');
     const geo = await fetch (`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`)
     if (!geo.ok) {
         throw new Error('Failed to fetch geocoding data');
@@ -13,13 +14,16 @@ export async function getWeatherData(city) {
     const lat = geoData[0].lat;
     const lon = geoData[0].lon;
 
-     
-    try {
+        if (!key ) throw new Error('API key is missing');
         const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${key}`);
         if (!response.ok) {
             throw new Error('Failed to fetch weather data');
         }
         const data = await response.json();
+
+        if (!data.timezone) {
+            throw new Error('No weather data found for the specified location');
+        }
         console.log(data);
         
         return data;
